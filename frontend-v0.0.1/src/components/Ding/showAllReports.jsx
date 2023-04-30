2924f82df76b283b4fa32495adadf7a13ca8d0b2
HkhCode <hkh.2003.1381@gmail.com>
2023-04-30 12:18:20 +0330

    7 tips changed

--- frontend-v0.0.1/src/components/Ding/showAllReports.jsx
+++ frontend-v0.0.1/src/components/Ding/showAllReports.jsx
@@ -3,11 +3,33 @@ import React, { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import Aside from "./Aside";
 import './styles/showAllReports.css';
-const months = ["فروردین"  , "اردیبهشت" , "خرداد" , "تیر" , "مرداد" , "شهریور" , "مهر" , "آبان", "آذر" , "دی" , "بهمن" , "اسفند"]
+const months = ["فروردین", "اردیبهشت", "خرداد", "تیر", "مرداد", "شهریور", "مهر", "آبان", "آذر", "دی", "بهمن", "اسفند"];
+const weekDays = ["شنبه" , "یک شنبه" , "دوشنبه" , "سه شنبه" , "چهارشنبه" , "پنج شنبه" , "جمعه"];
 const baseUrl = "http://localhost:5015/api/";
 const ShowAllReports = () => {
     const navigate = useNavigate();
-    const [preferentialReports, setPreferentialReports] = useState([]);
+    const [preferentialReports, setPreferentialReports] = useState([{
+        date: "1400/02/02",
+        inHour: "12:00",
+        outHour: "14:00",
+        workedTime: "02:00"
+    }, {
+        date: "1400/03/02",
+        inHour: "12:00",
+        outHour: "14:00",
+        workedTime: "02:00"
+    }, {
+        date: "1400/04/02",
+        inHour: "12:00",
+        outHour: "14:00",
+        workedTime: "02:00"
+    }, {
+        date: "1400/05/02",
+        inHour: "12:00",
+        outHour: "14:00",
+        workedTime: "02:00"
+    },
+    ]);
     const location = useLocation();
     const startDate = location.state.start;
     const endDate = location.state.end;
@@ -28,20 +50,20 @@ const ShowAllReports = () => {
                 </div>
             </div>
             <div className="aside">
-                <Aside index={2}/>
+                <Aside index={2} />
             </div>
 
             <div className="reportPartsContainer">
-            <div className="showAllReportsUserDetails">
-                <p> <span style={{fontWeight:"bold"}}>نام : </span> <span style={{float : "left" }}> حسین خادم</span></p>
-                <p> <span style={{fontWeight:"bold"}}>تاریخ : </span> <span style={{float : "left" }}>1400-2-2</span></p>
-            </div>
+                <div className="showAllReportsUserDetails">
+                    <p> <span style={{ fontWeight: "bold" }}>نام : </span> <span style={{ float: "left" }}> حسین خادم</span></p>
+                    <p> <span style={{ fontWeight: "bold" }}>تاریخ : </span> <span style={{ float: "left" }}>1400-2-2</span></p>
+                </div>
                 {
                     preferentialReports.length == 0 ? <h1>No DATA</h1> : preferentialReports.map((item) => {
                         return (<div className="reportPart">
                             <div className="ReportDate">
-                                <p>{item.date.slice(0,4)} {months[parseInt(item.date.slice(5,7))-1]}</p>
-                                <p>{parseInt(item.date.slice(8 , 10))} ام</p>
+                                <p>{weekDays[item.date.slice(8)-1]} {parseInt(item.date.slice(8, 10))} {months[parseInt(item.date.slice(5, 7)) - 1]} {item.date.slice(0, 4)}</p>
+                                <p></p>
                             </div>
                             <div className="startAndEndDate">
                                 <p>ورود : {item.inHour}</p>
@@ -49,10 +71,14 @@ const ShowAllReports = () => {
                                     خروج : {item.outHour}
                                 </p>
                             </div>
+                            <div className="arrowButtonSection">
+                                {">"}
+                            </div>
                             <div className="sumWorkingHour">
                                 <p>مجموع ساعات کاری  </p>
                                 <p>{item.workedTime}</p>
                             </div>
+
                         </div>)
                     })
 
--- frontend-v0.0.1/src/components/Ding/summaryReport.jsx
+++ frontend-v0.0.1/src/components/Ding/summaryReport.jsx
@@ -1,29 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import './styles/summaryReports.css';
 import Aside from "./Aside";
 import { useNavigate } from "react-router-dom";
-
-
+import { motorcycle } from "fontawesome";
+const dateParser = (date) => {
+    // date : yyyy/mm/dd
+    let result;
+    result += date.slice(0 , 4);
+    result += " ";
+    let month = date.slice(5 , 7);
+    if (month === "01"){
+        result += "فروردین"
+    } else if(month === "02") {
+        result += "اردیبهشت"
+    } else if(month === "03") {
+        result += "خرداد"
+    } else if(month === "04") {
+        result += "تیر"
+    } else if(month === "05") {
+        result += "مرداد"
+    } else if(month === "06") {
+        result += "شهریور"
+    } else if(month === "07") {
+        result += "مهر"
+    } else if(month === "08") {
+        result += "آبان"
+    } else if(month === "09") {
+        result += "آذر"
+    } else if(month === "10") {
+        result += "دی"
+    } else if(month === "11") {
+        result += "بهمن"
+    } else if(month === "12") {
+        result += "اسفند"
+    } else {
+        alert("خطا در کامپایل نرم افزار")
+    }
+}
 const SummaryReport = () => {
+    const [date , setDate] = useState(dateParser("1400/02/02"))
     const dateTypeSetter = (type) => {
         console.log("OK")
         const navigate = useNavigate();
-        if(type === 1){
-    
-        } else if(type === 2){
-    
-        } else if(type === undefined){
+        if (type === 1) {
+
+        } else if (type === 2) {
+
+        } else if (type === undefined) {
             navigate("/report");
         } else {
             alert("خطا هنگام کامپایل نرم افزار");
         }
     }
-    {dateTypeSetter(undefined)}
+    { dateTypeSetter(undefined) }
     return (
         <React.Fragment>
             <div className="container">
                 <div className="aside">
-                    <Aside index={2}/>
+                    <Aside index={2} />
                 </div>
                 <div className="nav">
                     <div className="titles">
@@ -31,16 +65,24 @@ const SummaryReport = () => {
                     </div>
                 </div>
                 <div className="summaryReportSection">
-                    <div className="reportTakerFullName">نام کارمند</div>
-                    <div className="reportTakerFullNameValue">حسین خادم</div>
-                    <div className="reportTakerFullName">تاریخ درخواست</div>
-                    <div className="reportTakerFullNameValue">1400/2/2</div><hr />
+                    <div className="userAndReportsSection">
+                        <div className="reportTakerFullName">نام کارمند</div>
+                        <div className="reportTakerFullNameValue">حسین خادم</div>
+                        <div className="reportTakerFullName">تاریخ درخواست</div>
+                        <div className="reportTakerFullNameValue">اردیبهشت/1400</div>
+                    </div>
+
                     <div className="summaryReportDataRow">
                         <span className="summaryReportDataRowTitle">مجموع ساعات طول شیفت</span>
                         <span className="summaryReportDataRowData">00:00</span>
                     </div>
                     <div className="summaryReportDataRow">
-                        <span className="summaryReportDataRowTitle">مجموع ساعات طول شیفت</span>
+                        <span className="summaryReportDataRowTitle">مجموع ساعات کارکرد</span>
+                        <span className="summaryReportDataRowData">00:00</span>
+                    </div>
+
+                    <div className="summaryReportDataRow">
+                        <span className="summaryReportDataRowTitle">مجموع ساعات تاخیر در ورود</span>
                         <span className="summaryReportDataRowData">00:00</span>
                     </div>
 
@@ -50,16 +92,34 @@ const SummaryReport = () => {
                     </div>
 
                     <div className="summaryReportDataRow">
-                        <span className="summaryReportDataRowTitle">مجموع ساعات طول شیفت</span>
+                        <span className="summaryReportDataRowTitle">مجموع ساعات تعجیل در خروج</span>
                         <span className="summaryReportDataRowData">00:00</span>
                     </div>
-
                     <div className="summaryReportDataRow">
-                        <span className="summaryReportDataRowTitle">مجموع ساعات طول شیفت</span>
+                        <span className="summaryReportDataRowTitle">مجموع ساعات غیبت</span>
+                        <span className="summaryReportDataRowData">00:00</span>
+                    </div>
+                    <div className="summaryReportDataRow">
+                        <span className="summaryReportDataRowTitle">مجموع ساعات اضافه کار</span>
+                        <span className="summaryReportDataRowData">00:00</span>
+                    </div>
+                    <div className="summaryReportDataRow">
+                        <span className="summaryReportDataRowTitle">مجموع ساعات ماموریت</span>
+                        <span className="summaryReportDataRowData">00:00</span>
+                    </div>
+                    <div className="summaryReportDataRow">
+                        <span className="summaryReportDataRowTitle">مجموع روز های ماموریت</span>
+                        <span className="summaryReportDataRowData">00:00</span>
+                    </div>
+                    <div className="summaryReportDataRow">
+                        <span className="summaryReportDataRowTitle">مجموع ساعات مرخصی ساعتی</span>
+                        <span className="summaryReportDataRowData">00:00</span>
+                    </div>
+                    <div className="summaryReportDataRow" style={{borderBottom : "1px solid rgb(235, 237, 237)"}}>
+                        <span className="summaryReportDataRowTitle">مجموع روز های مرخصی</span>
                         <span className="summaryReportDataRowData">00:00</span>
                     </div>
                 </div>
-
             </div>
         </React.Fragment>
     )
